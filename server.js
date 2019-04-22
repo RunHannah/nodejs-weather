a39190eb34ee53603405da09f440217d7f1d9980
Hannah Lee <leexhannah@gmail.com>
2019-04-21 18:09:17 -0700

    Refactor routing to use path module and express router.

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+const router = express.Router();
+
+// app.get('', (req, res) => {
+//   res.send({
+//     title: 'Check the weather',
+//     dateToday: new Date()
+//   });
+// });
+
+// app.get('*', (req, res) => {
+//   res.send({
+//     title: '404',
+//     errorMessage: 'Page not found.'
+//   });
+// });
+
+router.get('/', (res, req) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+// Setup static directory to serve
+app.use(express.static(__dirname + '/views'));
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000.');
-});
-
-app.get('', (req, res) => {
-  res.send({
-    title: 'Check the weather',
-    dateToday: new Date()
-  });
-});
-
-app.get('*', (req, res) => {
-  res.send({
-    title: '404',
-    errorMessage: 'Page not found.'
-  });
 });
